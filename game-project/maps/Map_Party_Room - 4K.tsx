bd04019069446b23f0bedede209f64a30c39441b
Alymcc <am12th@brocku.ca>
2025-11-09 02:09:01 -0500

    Set up Project Structure

---
+++ game-project/maps/Map_Party_Room - 4K.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Map_Party_Room" tilewidth="32" tileheight="32" tilecount="10920" columns="78">
+ <image source="Library/Mobile Documents/com~apple~CloudDocs/Map_Party_Room - 4k.png" width="2520" height="4482"/>
+</tileset>
